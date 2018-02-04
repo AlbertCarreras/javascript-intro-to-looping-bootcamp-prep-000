de3c40457a378c90be1d77cdeaf2f18a270a4c2f
Alberto Carreras <acarrerasc@gmail.com>
2018-02-04 20:42:59 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,5 +1,9 @@
 function forLoop(array) {
   for(var i=1; i < 25; i++) {
-    if array
+    if(i=1){
+      array.push("I'm a 1 strange loop.")
+    } else {
+      array.push("I'm ${i} strange loops")
+    }
   }
-  }
+}
