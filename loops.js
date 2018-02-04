5db72b0e59e695661f27c4c99c9dfb50eeeca511
Alberto Carreras <acarrerasc@gmail.com>
2018-02-04 21:50:04 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -21,7 +21,8 @@ function doWhileLoop(array) {
     return Math.random() >= 0.5
     }
   do {
-    console.log('doo-bee-doo-bee-doo')
+    array.pop()
   } 
-  while (maybeTrue());  
+  while (array.length > 0 && maybeTrue())
+  return array
 }
