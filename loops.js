eedf3fdc58f25719db8acc23299b3d78ab9f37eb
Alberto Carreras <acarrerasc@gmail.com>
2018-02-04 20:43:13 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,6 +1,6 @@
 function forLoop(array) {
   for(var i=1; i < 25; i++) {
-    if(i=1){
+    if(i === 1){
       array.push("I'm a 1 strange loop.")
     } else {
       array.push("I'm ${i} strange loops")
